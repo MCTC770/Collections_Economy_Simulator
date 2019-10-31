d286de25da55ddbd878b8dc23786389fc2019adf
DESKTOP-HCMRCN5\mc-tc <tomcolin.horn@gmail.com>
2019-10-31 01:12:52 +0100

    Refactored the looping

---
+++ Assets/Economy.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Economy : MonoBehaviour {
+
+	[SerializeField] float card1StarRarity;
+	[SerializeField] float card2StarRarity;
+	[SerializeField] float card3StarRarity;
+	[SerializeField] float card4StarRarity;
+	[SerializeField] float card5StarRarity;
+
+	[SerializeField] string[] cardRarityNames;
+
+	[SerializeField] float[] cardPackTier1;
+	[SerializeField] float[] cardPackTier2;
+	[SerializeField] float[] cardPackTier3;
+	[SerializeField] float[] cardPackTier4;
+
+	[SerializeField] float[] Room1;
+	[SerializeField] float[] Room2;
+	[SerializeField] float[] Room3;
+	[SerializeField] float[] Room4;
+	[SerializeField] float[] Room5;
+	[SerializeField] float[] Room6;
+	[SerializeField] float[] Room7;
+	[SerializeField] float[] Room8;
+	[SerializeField] float[] Room9;
+	[SerializeField] float[] Room10;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
